4c05d86e2ec84c16bff7f7fb3f4f62cf766f3c98
Nguyễn Đồng Kiên <94209892+KienNguyen1911@users.noreply.github.com>
2023-11-11 05:26:56 +0000

    ui: first setup

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-5xl">Hello Index</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
+
